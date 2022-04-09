93d59da8a2715fe50d9e580e127ab84db4f7654e
ScaerieTale <7010800+ScaerieTale@users.noreply.github.com>
2022-04-09 10:01:53 -0500

    New difficulty added
    New difficulty added.  "Easy" is now "Medium" and a new Easy difficulty has been added which sets the random number to between 1 and 50

--- main.py
+++ main.py
@@ -5,12 +5,23 @@ def game_loop():
     guesses = 0
     game_started = False
     while game_started == False:
-        difficulty = input("Easy or Hard? ").lower()
+        difficulty = input("""Easy, Medium, or Hard?
+        Easy: 10 guesses, and the number can only be between 1 and 50
+        Medium: 10 guesses, and the number will be between 1 and 100
+        Hard: 5 guesses.
+        What shall it be? """).lower()
         if difficulty == "easy":
+            guesses = 10
+            game_started = True
+            rng = random.randint(1, 50)
+        elif difficulty == "medium":
             guesses = 10
             game_started = True
         elif difficulty == "hard":
             guesses = 5
+            game_started = True
+        elif difficulty == "spam":
+            guesses == 100
             game_started = True
         else:
             print("I didn't understand that.")
