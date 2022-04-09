d11dd1d481e173dd49a48c3795228079a5219070
ScaerieTale <7010800+ScaerieTale@users.noreply.github.com>
2022-04-09 09:31:02 -0500

    Game "backbone" loop in place
    The game's backbone loop is in place and working.  Number of guesses ticks down per guess, and difficulty is functioning.  Just need to create the lose condition now :)

--- main.py
+++ main.py
@@ -2,14 +2,26 @@ import random
 rng = random.randint(1, 100)
 guesses = 0
 game_started = False
-difficulty = input("Easy or Hard? ").lower()
-if difficulty == "easy":
-    guesses = 10
-    game_started = True
-elif difficulty == "hard":
-    guesses = 5
-    game_started = True
-else:
-    print("I didn't understand that.")
+while game_started == False:
+    difficulty = input("Easy or Hard? ").lower()
+    if difficulty == "easy":
+        guesses = 10
+        game_started = True
+    elif difficulty == "hard":
+        guesses = 5
+        game_started = True
+    else:
+        print("I didn't understand that.")
 while game_started == True:
-    player_guess = input(f"Guess the number!  You have {guesses} attempts left) ")
+    if guesses == 0:
+        game_started = False
+    player_guess = int(input(f"Guess the number!  You have {guesses} attempts left) "))
+    if player_guess > rng:
+        guesses -= 1
+        print(f"Too high! ({guesses} attempts left)")
+    elif player_guess < rng:
+        guesses -= 1
+        print(f"Too low! ({guesses} attempts left)")
+    elif player_guess == rng:
+        print("Correct!")
+        game_started == False
